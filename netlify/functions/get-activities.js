bd066efcbbf2ea347548ff34b67531ace7d82035
Jason Sutter <jasonsutter87@gmail.com>
2023-01-12 09:58:00 -0800

    updating function

--- netlify/functions/get-activities.js
+++ netlify/functions/get-activities.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 export const handler = async (event, context) => {
   const eventBody = JSON.parse(event.body)
-  const POKE_API = 'https://developer.nps.gov/api/v1/activities?api_key=aChgBlCacUHuXxwo6FnjXeXCvW9L4jYWdTGQLCXM' + eventBody.region
+  const POKE_API = 'https://developer.nps.gov/api/v1/activities?api_key=aChgBlCacUHuXxwo6FnjXeXCvW9L4jYWdTGQLCXM'
 
   const response = await fetch(POKE_API)
   const data = await response.json()
