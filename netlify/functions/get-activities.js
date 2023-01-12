e9581bc16a1ee14ae06fcfbe146b4781de2c7f58
Jason Sutter <jasonsutter87@gmail.com>
2023-01-12 09:50:55 -0800

    testing new endpoint

--- netlify/functions/get-activities.js
+++ netlify/functions/get-activities.js
@@ -1,17 +1,16 @@
 import fetch from 'node-fetch'
 
 export const handler = async (event, context) => {
-  // const eventBody = JSON.parse(event.body)
-  // const POKE_API = 'https://developer.nps.gov/api/v1/activities?api_key=aChgBlCacUHuXxwo6FnjXeXCvW9L4jYWdTGQLCXM' + eventBody.region
-  //
-  // const response = await fetch(POKE_API)
-  // const data = await response.json()
+  const eventBody = JSON.parse(event.body)
+  const POKE_API = 'https://developer.nps.gov/api/v1/activities?api_key=aChgBlCacUHuXxwo6FnjXeXCvW9L4jYWdTGQLCXM' + eventBody.region
+
+  const response = await fetch(POKE_API)
+  const data = await response.json()
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      // pokemon: data.pokemon_entries
-        message: 'Hello World!',
+      pokemon: data.pokemon_entries
     })
   }
 }
