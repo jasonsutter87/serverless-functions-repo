b3c8b1ad98aaadff1e77245460053d1b5c9ca9ce
Jason Sutter <jasonsutter87@gmail.com>
2023-01-12 10:15:11 -0800

    function change

--- netlify/functions/get-activities.js
+++ netlify/functions/get-activities.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
 const response = await fetch('https://developer.nps.gov/api/v1/activities?api_key=aChgBlCacUHuXxwo6FnjXeXCvW9L4jYWdTGQLCXM');
 const data = await response.json();
