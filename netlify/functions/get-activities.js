8f39f92dfdda748c6fbcb33341d5ce8df0d81a35
Jason Sutter <jasonsutter87@gmail.com>
2023-01-12 09:49:06 -0800

    trying this out

---
+++ netlify/functions/get-activities.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch'
+
+export const handler = async (event, context) => {
+  // const eventBody = JSON.parse(event.body)
+  // const POKE_API = 'https://developer.nps.gov/api/v1/activities?api_key=aChgBlCacUHuXxwo6FnjXeXCvW9L4jYWdTGQLCXM' + eventBody.region
+  //
+  // const response = await fetch(POKE_API)
+  // const data = await response.json()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      // pokemon: data.pokemon_entries
+        message: 'Hello World!',
+    })
+  }
+}
