c6ae8ccbd5f1d03a7101bf01eefba42e8530d63a
Jason Sutter <jasonsutter87@gmail.com>
2023-01-12 10:08:06 -0800

    updating function

--- netlify/functions/get-activities.js
+++ netlify/functions/get-activities.js
@@ -1,16 +1,6 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
-export const handler = async (event, context) => {
-  const eventBody = JSON.parse(event.body)
-  const POKE_API = 'https://developer.nps.gov/api/v1/activities?api_key=aChgBlCacUHuXxwo6FnjXeXCvW9L4jYWdTGQLCXM'
+const response = await fetch('https://developer.nps.gov/api/v1/activities?api_key=aChgBlCacUHuXxwo6FnjXeXCvW9L4jYWdTGQLCXM');
+const data = await response.json();
 
-  const response = await fetch(POKE_API)
-  const data = await response.json()
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      pokemon: data.pokemon_entries
-    })
-  }
-}
+console.log(data);
